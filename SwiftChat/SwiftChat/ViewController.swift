5ce78921e3a46437e1ced288ca0606f7ded84b38
Daniel No <mr.danielno@gmail.com>
2017-09-25 15:48:52 -0400

    initial project

---
+++ SwiftChat/SwiftChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftChat
+//
+//  Created by Daniel No on 9/25/17.
+//  Copyright © 2017 Daniel No. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
